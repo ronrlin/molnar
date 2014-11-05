6d844c001beaffc9990bba9629c10696186f3c5d
Ron Lin <ronrlin@gmail.com>
2014-11-04 21:29:19 -0800

    Enabling email

--- HTML/contact.php
+++ HTML/contact.php
@@ -1,6 +1,6 @@
 <?php 
-$emailTo = 'yourmail@example.com';
-$siteTitle = 'YourSiteTitle';
+$emailTo = 'ronrlin@gmail.com';
+$siteTitle = 'dezsomolnar.com';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
